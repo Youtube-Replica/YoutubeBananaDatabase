4f9d1605d1c2a155acdfe3bb381ce7571e025b93
Omar Abdelwahab <oagamingz@gmail.com>
2018-04-28 00:05:02 +0200

    - initial commit

--- src/main/java/DatabaseService.java
+++ src/main/java/DatabaseService.java
@@ -5,21 +5,24 @@ import commands.Command;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.postgresql.ds.PGPoolingDataSource;
 
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeoutException;
 
-public class UserService {
+public class DatabaseService {
 
-    private static final String RPC_QUEUE_NAME = "user-request";
-
+    private static final String RPC_QUEUE_NAME = "database-request";
+    private static PGPoolingDataSource source;
     public static void main(String [] argv) {
 
         //initialize thread pool of fixed size
         final ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(4);
+        initDBPool();
 
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
@@ -44,16 +47,19 @@ public class UserService {
                     System.out.println("Responding to corrID: "+ properties.getCorrelationId());
 
                     String response = "";
-
+                    java.sql.Connection conn = null;
                     try {
                         String message = new String(body, "UTF-8");
-                        Command cmd = (Command) Class.forName("commands."+getCommand(message)).newInstance();
+                        Command cmd = (Command) Class.forName("commands."+"Fake").newInstance();
+                        conn = source.getConnection();
+
                         HashMap<String, Object> props = new HashMap<String, Object>();
                         props.put("channel", channel);
                         props.put("properties", properties);
                         props.put("replyProps", replyProps);
                         props.put("envelope", envelope);
                         props.put("body", message);
+                        props.put("dbConnection", conn);
 
                         cmd.init(props);
                         executor.submit(cmd);
@@ -61,13 +67,16 @@ public class UserService {
                         System.out.println(" [.] " + e.toString());
                     } catch (IllegalAccessException e) {
                         e.printStackTrace();
-                    } catch (ParseException e) {
-                        e.printStackTrace();
                     } catch (InstantiationException e) {
                         e.printStackTrace();
                     } catch (ClassNotFoundException e) {
                         e.printStackTrace();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
                     } finally {
+                        if (conn != null) {
+                            try { conn.close(); } catch (SQLException e) {}
+                        }
                         synchronized (this) {
                             this.notify();
                         }
@@ -88,10 +97,14 @@ public class UserService {
 //        }
 
     }
-    public static String getCommand(String message) throws ParseException {
-        JSONParser parser = new JSONParser();
-        JSONObject messageJson = (JSONObject) parser.parse(message);
-        String result = messageJson.get("command").toString();
-        return result;
+    public static void initDBPool(){
+        source = new PGPoolingDataSource();
+        source.setDataSourceName("A Data Source");
+        source.setServerName("localhost");
+        source.setDatabaseName("scalable");
+        source.setUser("postgres");
+        source.setPassword("passw0rd");
+        source.setMaxConnections(10);
     }
+
 }
---
+++ src/main/java/commands/Fake.java
@@ -0,0 +1,110 @@
+package commands;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Envelope;
+import commands.Command;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.sql.*;
+import java.util.HashMap;
+import java.util.Properties;
+
+public class Fake extends Command {
+
+    public void execute() {
+        HashMap<String, Object> props = parameters;
+
+        Channel channel = (Channel) props.get("channel");
+        System.out.println( "Received: " + props.get("body"));
+        AMQP.BasicProperties properties = (AMQP.BasicProperties) props.get("properties");
+        AMQP.BasicProperties replyProps = (AMQP.BasicProperties) props.get("replyProps");
+        java.sql.Connection conn = (java.sql.Connection) props.get("dbConnection");
+        Envelope envelope = (Envelope) props.get("envelope");
+        String callStatement = "";
+        int outType;
+        JSONArray inputArray;
+        try {
+            JSONParser parser = new JSONParser();
+            JSONObject body = (JSONObject) parser.parse((String) props.get("body"));
+
+
+            callStatement = body.get("call_statement").toString();
+            outType =Integer.parseInt( body.get("out_type").toString());
+
+            inputArray = (JSONArray) body.get("input_array");
+            String response = callDatabase(callStatement,outType,inputArray, conn);
+            System.out.println("Sending... "+ response);
+            channel.basicPublish("", properties.getReplyTo(), replyProps, response.getBytes());
+            channel.basicAck(envelope.getDeliveryTag(), false);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public String callDatabase(String callStatement, int outType, JSONArray inputArray, java.sql.Connection conn){
+        String url = "jdbc:postgresql://localhost/scalable";
+        Properties props = new Properties();
+        props.setProperty("user", "postgres");
+        props.setProperty("password", "passw0rd");
+
+        JSONArray jsonArray = new JSONArray();
+        try {
+            conn = DriverManager.getConnection(url, props);
+            conn.setAutoCommit(false);
+            CallableStatement upperProc = conn.prepareCall(callStatement);
+            if(outType != 0){
+
+            upperProc.registerOutParameter(1,outType);
+            }
+
+            int i = 2;
+            for(int j=0;j < inputArray.size();j++){
+
+                JSONObject o = (JSONObject) inputArray.get(j);
+                int type = Integer.parseInt(o.get("type").toString());
+                switch (type){
+                    case Types.INTEGER :
+                        int value =  Integer.parseInt(o.get("value").toString());
+                        upperProc.setInt(i,value);
+                        break;
+                }
+                i++;
+            }
+            upperProc.execute();
+
+            switch (outType){
+                case Types.OTHER:
+                ResultSet rs = (ResultSet) upperProc.getObject(1);
+
+                ResultSetMetaData resultSetMetaData = rs.getMetaData();
+                while (rs.next()) {
+
+                    JSONObject json = new JSONObject();
+                    for(int j=1;j<resultSetMetaData.getColumnCount()+1;j++){
+                    switch (resultSetMetaData.getColumnType(j)){
+                        case Types.VARCHAR :
+                            System.out.println("call database");
+                            json.put(resultSetMetaData.getColumnName(j),rs.getString(j));
+                            break;
+                    }
+                    }
+                    jsonArray.add(json);
+                }
+                rs.close();
+                upperProc.close();
+            }
+         }catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return jsonArray.toString();
+    }
+}
